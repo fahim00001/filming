4c2d3ad4dffac873c49ea41d2367d3301d29a35b
Your git config --global user.name fahim00001git config --global user.name Your <fahimahmed000075@gmail.com>
2022-04-17 15:49:10 +0600

    carousel added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,46 @@
 import React from 'react';
-
+import { Carousel } from 'react-bootstrap';
+import bannerImg1 from '../../../images/banner/banner1.png'
+import bannerImg2 from '../../../images/banner/banner2.jpg'
+import bannerImg3 from '../../../images/banner/banner3.jpg'
 const Banner = () => {
     return (
-        <div>
-            
+        <div className='container-fluid h-50'>
+            <Carousel fade variant="dark">
+  <Carousel.Item interval={1000}>
+    <img
+      className="h-75 w-100"
+      src={bannerImg3}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+    <img
+      className="h-75 w-100"
+      src={bannerImg2}
+      alt="Second slide"
+    />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="h-75 w-100"
+      src={bannerImg1}
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
         </div>
     );
 };
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+    
+    return (
+        <div>
+            <h1 className='text-center my-5 '  >My services</h1>
+        </div>
+    );
+};
+
+export default Services;<h1 className='text-center my-5'  >My services</h1>

b80c3ec3ef1c708827ec0db25b0074d72f8a0562
Your git config --global user.name fahim00001git config --global user.name Your <fahimahmed000075@gmail.com>
2022-04-18 19:30:35 +0600

    about me added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import Mypic from '../../images/professionalpic.jpg'
 const About = () => {
     return (
-        <div>
-            <h1>this is about</h1>
+        <div className='vh-100'>
+            <div className='row mx-auto container' >
+                <div className='col-lg-7'>
+                    <h1 className='text-center' >About</h1>
+                    <p className='fs-5 ' >my name's fahim . MY childhood dream is to became an top class coder. At the of 12 i realise
+                        this thing give me a value.But for my laziness every time i failed.But last few month i'm learning web dev . Now i believe i can do.
+                        My 2022 is to become expert wev developer  
+                    </p>
+                </div>
+                <div className='col-lg-5'>
+                    <img className='img-fluid' src={Mypic} alt="" srcset="" />
+                </div>
+            </div>
+            
         </div>
     );
 };
--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>This is blog</h1>
+        <div className='container vh-100'>
+            <div>
+                <h3>Difference between authorization and authentication?</h3>
+                <p>simply authentication is verify user.Means if any user want's to login website or any app .User have to authentication first .He have to put some information to authenticate .</p>
+            </div>
+            <div></div>
         </div>
     );
 };
